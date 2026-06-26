c1005a7b9f4fa85ec144fe45df889c93bd1104f4
rishyandhark-sudo <rishyandhark@gmail.com>
2026-06-26 11:19:48 +0530

    Refactor checkGuess function and variable declarations

--- script.js
+++ script.js
@@ -3,15 +3,10 @@ let attempts = 0;
 
 function checkGuess() {
 
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-let attempts = 0;
-
-function checkGuess() {
     let guessInput = document.getElementById("guessInput");
     let guess = guessInput.value;
     let message = document.getElementById("message");
 
-    // Check if input is empty
     if (guess === "") {
         message.innerText = "⚠️ Please enter a number!";
         return;
@@ -30,17 +25,19 @@ function checkGuess() {
         message.innerText = "📈 Too High!";
     }
     else {
+
         message.innerText =
             "🎉 Correct! You guessed it in " + attempts + " attempts.";
 
         setTimeout(function () {
+
             randomNumber = Math.floor(Math.random() * 100) + 1;
             attempts = 0;
 
             document.getElementById("attempts").innerText = "Attempts: 0";
             guessInput.value = "";
-            message.innerText = "🔄 New number generated! Start guessing...";
+            message.innerText = "🔄 New number generated! Start guessing!";
+
         }, 3000);
     }
-}
 }
